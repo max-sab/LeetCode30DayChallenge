ca555267bcdc273462d02d787109cf5558da1ffe
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-04-01 17:39:21 +0300

    Initial Commit

---
+++ LeetCode30DaysChallenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode30DaysChallenge
+//
+//  Created by Maksym Sabadyshyn on 4/1/20.
+//  Copyright © 2020 Maksym Sabadyshyn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
