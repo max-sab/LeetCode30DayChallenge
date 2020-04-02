0a496f1e5ac3d541532e33c274faa4ab64d6db18
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-04-02 10:12:43 +0300

    Starting template for day 2 of Leet Code Challenge

---
+++ LeetCode30DaysChallenge/DayTwo.swift
@@ -0,0 +1,15 @@
+//
+//  DayTwo.swift
+//  LeetCode30DaysChallenge
+//
+//  Created by Maksym Sabadyshyn on 4/2/20.
+//  Copyright © 2020 Maksym Sabadyshyn. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    func isHappy(_ n: Int) -> Bool {
+
+    }
+}
