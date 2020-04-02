80ea3e6d2802686116d960a068fcbf1d29bd4dfa
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-04-02 09:41:19 +0300

    approach changed since func. approach wasn't accepted

--- LeetCode30DaysChallenge/DayOne.swift
+++ LeetCode30DaysChallenge/DayOne.swift
@@ -11,15 +11,39 @@ import Foundation
 class DayOne {
     var res: Int?
     var usedNumbers = Set<Int>()
+    var candidates = Set<Int>()
 
     func singleNumber(_ nums: [Int]) -> Int {
-        nums.forEach {
-            num in
-            if nums.filter({$0 == num}).count == 1 {
-                res = num
+        for num in nums {
+            if let res = res {
+                if res == num {
+                    self.res = nil
+                    usedNumbers.insert(num)
+                    candidates.remove(num)
+                } else if candidates.contains(num) {
+                    candidates.remove(num)
+                } else {
+                    candidates.insert(num)
+                }
+            } else {
+                if !usedNumbers.contains(num) {
+                    self.res = num
+                    candidates.remove(num)
+                }
+                usedNumbers.insert(num)
             }
         }
+
+        if !candidates.isEmpty {
+            res = candidates.first
+        }
+
+        print(candidates)
+
         //forced unwrapped because we always have result by task definition
         return res!
     }
+    
 }
+
+
