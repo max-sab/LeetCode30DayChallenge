8ef1af15b1e78de54c4326410e43c2731b4a67e7
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-04-02 15:23:54 +0300

    Day two challenge made

--- LeetCode30DaysChallenge/DayTwo.swift
+++ LeetCode30DaysChallenge/DayTwo.swift
@@ -8,8 +8,46 @@
 
 import Foundation
 
-class Solution {
+class DayTwo {
+    var sum = 0
+    var prevSums = [Int]()
     func isHappy(_ n: Int) -> Bool {
+        var digits = getDigits(for: n)
+        digits.forEach {
+            digit in
+            sum += Int(pow(Double(digit), 2))
+        }
+        prevSums.append(sum)
 
+        while sum != 1 {
+            digits = getDigits(for: sum)
+            sum = 0
+            digits.forEach {
+                digit in
+                sum += Int(pow(Double(digit), 2))
+            }
+            if prevSums.contains(sum) {
+                return false
+            }
+            prevSums.append(sum)
+
+        }
+
+        return true
+    }
+
+    func getDigits(for num: Int) -> [Int] {
+        var currentNum = num
+        var digits = [Int]()
+
+        if num < 10 {
+            return [num]
+        }
+        repeat {
+            digits.append(currentNum % 10)
+            currentNum /= 10
+        } while currentNum >= 10
+        digits.append(currentNum % 10)
+        return digits
     }
 }
