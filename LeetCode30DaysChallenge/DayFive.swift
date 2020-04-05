a672998ba8a75113e3f700d6a1a26d7d63732d69
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-04-05 22:28:42 +0300

    template made

---
+++ LeetCode30DaysChallenge/DayFive.swift
@@ -0,0 +1,24 @@
+//
+//  DayFive.swift
+//  LeetCode30DaysChallenge
+//
+//  Created by Maksym Sabadyshyn on 4/5/20.
+//  Copyright © 2020 Maksym Sabadyshyn. All rights reserved.
+//
+
+import Foundation
+
+struct DayFive {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var topToBuy = prices.sorted()
+        var maxProfit = 0
+        var buyMode = true
+
+        prices.forEach{
+
+        }
+
+
+        return 0
+    }
+}
